d505ad804a0aca75a26bc5f8016cb981590434bf
AnushaSrinivasa <87950839+AnushaSrinivasa@users.noreply.github.com>
2021-09-01 14:58:32 -0700

    Update BinaryGapIteration.js

--- BinaryGapIteration.js
+++ BinaryGapIteration.js
@@ -1,7 +1,8 @@
 let largestBinaryGap = (num) => {
     if (num === parseInt(num, 10) && num > 0 && num <= 2147483647) {
-        const binaryNum = (num >>> 0).toString(2);
+        const binaryNum = (num >>> 0).toString(2); //convert numb to binary
         let largestCount = 0, count = 0;
+        //Iterate binary and start counting when we find 0, reset count when we find 1
         for (let i=0; i<binaryNum.length; i++) {
             if(binaryNum[i] === '0') {
                 count++;
